ee46d9eaf84a2c113ce03c270c58a988610af2bf
Nigel P <58761175+Nigleto@users.noreply.github.com>
2021-04-06 21:34:15 -0700

    j

--- other/main2.py
+++ other/main2.py
@@ -8,21 +8,22 @@ def getdata(stock):
     company_quote = company_quote.json()
     share_price = float("{0:.2f}".format(company_quote[0]['price']))
     # Balance Sheet Group of Items
-    BS = requests.get(f"https://financialmodelingprep.com/api/v3/financial/balance-sheet-statement/{stock}?period=quarter")
+    BS = requests.get(
+        f"https://financialmodelingprep.com/api/v3/financial/balance-sheet-statement/{stock}?period=quarter")
     BS = BS.json()
 
     # Total Debt
     debt = float("{0:.2f}".format(float(BS['financials'][0]['Total debt']) / 10 ** 9))
 
     # Total Cash
-    cash = float("{0:.2f}".format(float(BS['financials'][0]['Cash and Short-Term Investments'])/10**9))
+    cash = float("{0:.2f}".format(float(BS['financials'][0]['Cash and Short-Term Investments']) / 10 ** 9))
 
     # Income Statement Group of Items
     IS = requests.get(f"https://financialmodelingprep.com/api/v3/financial/income-statement/{stock}?period=quarter")
     IS = IS.json()
 
     # Most Recent Quarterly Revenue
-    qRev = float("{0:.2f}".format(float(IS['financials'][0]['Revenue'])/10))
+    qRev = float("{0:.2f}".format(float(IS['financials'][0]['Revenue']) / 10))
 
     # Company Profile Group of Items
     company_info = requests.get(f"https://financialmodelingprep.com/api/v3/company/profile/{stock}")
@@ -32,6 +33,7 @@ def getdata(stock):
     ceo = company_info['profile', ['ceo']]
 
     return share_price, cash, debt, qRev, ceo
+
 
 tickers = ('AAPL', 'MSFT', 'GOOG', 'T', 'CSCO', 'INTC', 'ORCL', 'AMZN', 'FB', 'TSLA', 'NVDA')
 data = map(getdata, tickers)
