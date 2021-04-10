da2da6c8465cf3db215e5ee1276c82d23a087540
Nigel P <58761175+Nigleto@users.noreply.github.com>
2021-04-10 10:07:25 -0700

    fdgfd

--- GUI.py
+++ GUI.py
@@ -3,21 +3,15 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/<name>")
-def user(name):
-    return render_template("index.html", content=name)
-    #return f"Hello {name}!"
-
 
 @app.route("/admin")
 def admin():
     return 'hello banana'
-        #redirect(url_for("user", name='Admin'))
 
 
 @app.route("/")
-def home(name):
-    return render_template("index.html", content=name)
+def home():
+    return render_template("index.html", content='hello', r=3)
 
 
 if __name__ == "__main__":
