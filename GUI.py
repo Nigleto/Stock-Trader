a65f6e5669ba6a21c2c9a81454cf964f665a22be
Nigel P <58761175+Nigleto@users.noreply.github.com>
2021-04-11 22:07:37 -0700

    m

--- GUI.py
+++ GUI.py
@@ -13,5 +13,15 @@ def home():
     return render_template("index.html", content='hello', r=3)
 
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/<user>")
+def user(user):
+    return f"<h1>{user}</h1>"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
